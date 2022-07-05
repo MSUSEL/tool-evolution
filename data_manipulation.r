fb9279ee87d374a92fc46e98f6180f0bd266ab17
thetravisweber <tbroweber@gmail.com>
2022-07-05 11:03:26 -0600

    first graphs, lets gooooooo baby

--- data_manipulation.r
+++ data_manipulation.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rjson)
+require(reshape2)
 
 # Give the input file name to the function.
 cve_bin_list <- fromJSON(file = "./cve-bin-tool-runner/data_outputs/full_results_1655757361.json")
@@ -8,3 +9,11 @@ cve_bin_dataframe <- lapply(cve_bin_list, as.data.frame)
 
 cve_bin_dataframe2 <- do.call(rbind, cve_bin_dataframe)
 
+avgs <- apply(cve_bin_dataframe2, 2, mean)
+
+avgs_df <- as.data.frame(avgs)
+
+avgs_df$version <- rownames(avgs_df)
+
+ggplot(avgs_df, aes(x = version, y=avgs)) +
+  geom_point()
