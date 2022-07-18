ed5c8778142b21c81382a4284189c4e9289e1651
thetravisweber <tbroweber@gmail.com>
2022-07-18 17:17:27 -0600

    refactored heatmap

--- 03_analysis/03_incremental/version_heatmap.r
+++ 03_analysis/03_incremental/version_heatmap.r
@@ -1,28 +1,27 @@
 library(plyr); library(dplyr)
 
-my_palette <- colorRampPalette(c("red", "black", "green"))(n = 299)
-
 # heat map but normalized between zero and 1
-cwe_checker %>% select(starts_with("version")) %>%
-  apply(2, function(x) (x-min(x))/(max(x)-min(x))) %>%
-  apply(2, as.numeric) %>%
-  as_tibble() %>%
-  # sort by vulnerabilities in first version
+cwe_checker %>% apply_standardization() %>%
+  rev() %>%
   arrange(version_0.6) %>%
-  rev() %>%
-  # remove filename column
-  select(-starts_with("filename")) %>% 
-  as.matrix() %>% 
-  heatmap.2(Rowv = F, Colv = F, tracecol = NA, col=my_palette, labRow = F)
+  plot_as_heatmap()
 
-cve_bin %>% select(starts_with("version")) %>%
-  apply(2, function(x) (x-min(x))/(max(x)-min(x))) %>%
-  apply(2, as.numeric) %>%
-  as_tibble() %>%
-  # sort by vulnerabilities in first version
+cve_bin %>% apply_standardization() %>%
   arrange(version_3.1.1) %>%
-  rev() %>%
-  # remove filename column
-  select(-starts_with("filename")) %>% 
-  as.matrix() %>% 
-  heatmap.2(Rowv = F, Colv = F, tracecol = NA, col=my_palette, labRow = F)
+  plot_as_heatmap()
+  
+  
+apply_standardization <- function(d) {
+  d %>% select(starts_with("version")) %>%
+    apply(2, function(x) (x-min(x))/(max(x)-min(x))) %>%
+    apply(2, as.numeric) %>%
+    as_tibble()
+}  
+
+plot_as_heatmap <- function(d) {
+  my_palette <- colorRampPalette(c("red", "black", "green"))(n = 299)
+  
+  d %>% select(-starts_with("filename")) %>% 
+    as.matrix() %>% 
+    heatmap.2(Rowv = F, Colv = F, tracecol = NA, col=my_palette, labRow = F)
+}
