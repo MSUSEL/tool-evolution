de6b3194b87fde7bdee0ef870d9f73dfc85b6ac8
thetravisweber <tbroweber@gmail.com>
2022-07-18 15:37:36 -0600

    added time stamp to wrangling

--- 02_wrangling/02_protocol/cve_bin_tool.r
+++ 02_wrangling/02_protocol/cve_bin_tool.r
@@ -9,6 +9,10 @@ cve_bin <- fromJSON(file = sprintf("./01_acquisition/04_product/cve_bin_tool_res
 # remove data about version 3.1
 cve_bin <- cve_bin[, names(cve_bin)!="version_3.1"]
 
+cve_bin <- fromJSON(file = sprintf("./01_acquisition/04_product/cve_bin_tool_results_%d.json", use_date)) %>% 
+  lapply(as.data.frame) %>%
+  do.call(rbind, .)
+
 # pull in release date data
 version_dates <- read.csv("./02_wrangling/01_input/cve_bin_tool_release_dates.csv")
 version_dates <- t(version_dates)
@@ -28,5 +32,7 @@ cve_bin_long <- cve_bin %>%
 cve_bin_long <- left_join(cve_bin_long, dates)
 
 # save as csv
-write.csv(cve_bin, "./02_wrangling/04_product/cve_bin_tool_wide.csv", row.names = F)
-write.csv(cve_bin_long, "./02_wrangling/04_product/cve_bin_tool_long.csv", row.names = F)
+sprintf("./02_wrangling/04_product/cve_bin_tool_wide_%d.csv", use_date) %>%
+  write.csv(cve_bin, ., row.names = F)
+sprintf("./02_wrangling/04_product/cve_bin_tool_long_%d.csv", use_date) %>%
+  write.csv(cve_bin_long, ., row.names = F)
