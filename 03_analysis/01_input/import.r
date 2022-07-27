066bb643f7ce00bcb221292fcd1c43cd74ad6cdf
Travis Weber <travis@travis.gg>
2022-07-27 10:24:30 -0600

    Revert "New structure"

--- 03_analysis/01_input/import.r
+++
@@ -1,5 +0,0 @@
-cve_bin <- read.csv("./02_wrangling/04_product/cve_bin_tool_wide_1658175857.csv")
-cve_bin_long <- read.csv("./02_wrangling/04_product/cve_bin_tool_long_1658175857.csv")
-
-cwe_checker <- read.csv("./03_analysis/01_input/csvs_final_data/cwe_checker_wide.csv")
-cwe_checker_long <- read.csv("./03_analysis/01_input/csvs_final_data/cwe_checker_long.csv")
