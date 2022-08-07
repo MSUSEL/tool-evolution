5bbd681aeeb6d609636939d37c5b4e63357f7eda
Colleen Lemak <clemak11@gmail.com>
2022-08-06 20:13:40 -0700

    produced data with modified scanning

--- 01_acquisition/02_protocol/cwe-checker-runner.py
+++ 01_acquisition/02_protocol/cwe-checker-runner.py
@@ -1,18 +1,17 @@
 '''
-- This file called utils.py which generates the cwe_checker output
+- This file called utils.py which generates the cwe_checker output.
 
 - Our goal is to count the number of vulnerabilities in the binary files, and compare this data 
 against other static analysis tools.
 '''
 
-from pytest import ExitCode
 import utils
 import os
 import json
 
 # directory with all binaries
 # file_path = "./01_acquisition/01_input/binaries"
-file_path = r"C:\Users\clema\REU_2022\tool-evolution\01_acquisition\01_input\test_bins"
+file_path = r"C:\Users\clema\REU_2022\tool-evolution\01_acquisition\01_input\binaries"
 files = os.listdir(file_path)
 
 # write names of binary files to "binaries.json"
