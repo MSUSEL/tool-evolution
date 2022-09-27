22f260dc18917a834c727312f303474ed6634214
Ann Marie Reinhold <ann.marie.reinhold@gmail.com>
2022-09-27 12:29:17 -0600

    fixed dumb mistake where wrote files to wrong path

--- 02_wrangling/02_protocol/cwe_checker_process_json.r
+++ 02_wrangling/02_protocol/cwe_checker_process_json.r
@@ -46,11 +46,11 @@ cwe_check_long <-
 
 # write the data to csv
 # specify the path where csv should be stored
-pthout <- "../04_product/"
+pthout <- "../04_product"
 # specify file names
 widenm <- paste0("cwe_check_wide_", run_idx, ".csv")
 longnm <- paste0("cwe_check_long_", run_idx, ".csv")
 # write to file
-write.csv(cwe_check, paste0(pthout, widenm), row.names = FALSE)
-write.csv(cwe_check_long, paste0(pthout, longnm), row.names = FALSE)
+write.csv(cwe_check, paste0("/", pthout, widenm), row.names = FALSE)
+write.csv(cwe_check_long, paste0("/",pthout, longnm), row.names = FALSE)
 
