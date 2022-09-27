c03d77834c470c494c8646960a89fa2195f8cefb
Ann Marie Reinhold <ann.marie.reinhold@gmail.com>
2022-09-27 13:40:37 -0600

    minor changes, added libraries, etc

--- 04_analysis/02_protocol/alpha_graphs_through_time.R
+++ 04_analysis/02_protocol/alpha_graphs_through_time.R
@@ -6,13 +6,17 @@ through_time_alpha <- function(d) {
   d %>%
     ggplot(aes(x = version, y = vuln_count)) +
     geom_line(mapping = aes(group = filename), color="black", alpha=0.2, size=2) +
-    labs(y = "Findings Count", x = "Version") + 
+    labs(y = "Findings Count", x = "Version") +
     theme(
       axis.text.x = element_text(angle = 40, hjust=1)
     )
 }
 
-cve_bin_long %>% through_time_alpha()
+grid.arrange(
+  cve_bin_long %>% through_time_alpha(),
+  cwe_checker_long %>% through_time_alpha(),
+  nrow = 2
+)
 
-cwe_checker_long %>% through_time_alpha()
-  
+
+
--- 04_analysis/03_incremental/version_heatmap.r
+++ 04_analysis/03_incremental/version_heatmap.r
@@ -1,17 +1,17 @@
-library(plyr); library(dplyr)
+library(plyr); library(dplyr); library(gplots)
 
 apply_standardization <- function(d) {
   d %>% select(starts_with("version")) %>%
     apply(2, function(x) (x-min(x))/(max(x)-min(x))) %>%
     apply(2, as.numeric) %>%
     as_tibble()
-}  
+}
 
 plot_as_heatmap <- function(d) {
   my_palette <- colorRampPalette(c("red", "black", "green"))(n = 299)
-  
-  d %>% select(-starts_with("filename")) %>% 
-    as.matrix() %>% 
+
+  d %>% select(-starts_with("filename")) %>%
+    as.matrix() %>%
     heatmap.2(Rowv = F, Colv = F, tracecol = NA, col=my_palette, labRow = F)
 }
 
@@ -24,6 +24,6 @@ cwe_checker %>% apply_standardization() %>%
 cve_bin %>% apply_standardization() %>%
   arrange(version_3.1.1) %>%
   plot_as_heatmap()
-  
-  
+
+
 
