a4d4955cd96acda19bf25f94c414e5108c406a76
Ann Marie Reinhold <ann.marie.reinhold@gmail.com>
2022-09-27 13:27:49 -0600

    updated setup file to point to correct cwe checker outputs

--- 04_analysis/01_input/setup.r
+++ 04_analysis/01_input/setup.r
@@ -1,10 +1,9 @@
-# this file should import all data needed to make all product and 
+# this file should import all data needed to make all product and
 # incremental graphs
 
 cve_bin <- read.csv("./02_wrangling/04_product/cve_bin_tool_wide_1658175857.csv")
 cve_bin_long <- read.csv("./02_wrangling/04_product/cve_bin_tool_long_1658175857.csv")
 
-cwe_checker <- read.csv("./04_analysis/01_input/cwe_checker_wide.csv") %>%
-  select(-X)
-cwe_checker_long <- read.csv("./04_analysis/01_input/cwe_checker_long.csv") %>%
-  select(-X)
+cwe_checker <- read.csv("./02_wrangling/04_product/cwe_check_wide_1664248617.csv")
+cwe_checker_long <- read.csv("./02_wrangling/04_product/cwe_check_long_1664248617.csv")
+names(cwe_checker_long)[names(cwe_checker_long) == "findings_count"] <- "vuln_count"
